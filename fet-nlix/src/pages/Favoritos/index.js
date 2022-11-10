06725a44fff7a6c5667139aeffbffeb12c861ac7
Matheus Maica <matheus_maica@sicredi.com.br>
2022-11-10 11:46:03 -0300

    criando lista de favoritos

--- fet-nlix/src/pages/Favoritos/index.js
+++ fet-nlix/src/pages/Favoritos/index.js
@@ -1,9 +1,33 @@
 import './favoritos.css'
+import {  useEffect, useState} from 'react'
+import { Link } from 'react-router-dom'
 function Favoritos(){
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(()=>{
+        const  minhaLista = localStorage.getItem("@fletNix")
+        setFilmes(JSON.parse(minhaLista) || []);
+
+    },[])
     return(
-        <>
-            <h3>Favoritos</h3>
-        </>
+        <div className='meus-filmes'>
+            <h1 >Meus filmes</h1>
+
+            <ul>
+                {filmes.map((filme)=>{
+                    return(
+                       <li key ={filme.id}>
+                            <span>{filme.title}</span>
+                            <div>
+                                <Link to={`/filme/${filme.id}`}>Ver detalhes</Link>
+                                <button>Excluir</button>
+                            </div>
+
+                       </li> 
+                    )
+                })}
+            </ul>
+        </div>
     )
 }
 
